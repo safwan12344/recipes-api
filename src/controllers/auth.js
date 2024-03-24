3ef25ca931b7713e6fe6a6d382fae560939c8b51
AA <safwan12344>
2024-03-24 19:45:53 +0200

    reset password

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken"
 import User from "../models/user"
 import { authenticate } from "../middlerwares/auth"
 import { createError } from "../utils/create-error"
+import { sendEmail } from "../utils/email"
 
 const login = async function (req, res, next) {
   const data = { ...req.body }
@@ -35,7 +36,74 @@ const me = [
   },
 ]
 
+const forgotPassword = async (req, res, next) => {
+  try {
+    const email = req.body.email
+    const user = await User.findOne({ email: email })
+    if (!user) {
+      return res.status(200).json()
+    }
+    const forgotPasswordToken = jwt.sign(
+      { _id: user._id, email: user.email },
+      process.env.TOKEN_SECRET,
+      { expiresIn: "5m" }
+    )
+    const mailOptions = {
+      from: process.env.GMAIL_USER,
+      to: user.email,
+      subject: `Reset Password`,
+      text: `Hi ${user.firstName},
+
+      We noticed you're having trouble accessing your account. No worries, we're here to help!
+      
+      To reset your password, simply click the link below:
+      
+      ${process.env.CLIENT_URL}/reset-password/${forgotPasswordToken}
+      
+      If you didn't request this, please ignore this email — your account is safe and sound.
+      
+      Thanks,
+      RecipeWorldWide Team`,
+    }
+    await sendEmail(mailOptions)
+    res.status(204).json()
+  } catch (error) {
+    next(error)
+  }
+}
+
+const resetPassword = async (req, res, next) => {
+  try {
+    const token = req.params.token
+    jwt.verify(token, process.env.TOKEN_SECRET, async (err, userToken) => {
+      if (err) {
+        return next(createError(400, "invalid token"))
+      }
+
+      const user = await User.findOne({
+        _id: userToken._id,
+        email: userToken.email,
+      })
+
+      bcrypt.hash(req.body.password, 10, async function (err, hash) {
+        if (err) {
+          return next(
+            createError(400, "failed to update password. please try later!")
+          )
+        }
+        user.password = hash
+        await user.save()
+        res.status(200).json({ message: "password changed" })
+      })
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
 export default {
   login,
   me,
+  forgotPassword,
+  resetPassword,
 }
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,7 +3,8 @@ import authController from "../controllers/auth"
 const router = express.Router()
 
 router.post("/login", authController.login)
-
+router.post("/forgot-password", authController.forgotPassword)
+router.post("/reset-password/:token", authController.resetPassword)
 router.get("/me", authController.me)
 
 export default router
